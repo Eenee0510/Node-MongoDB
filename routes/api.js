1850cd48d9f1564066457f9368e9b5188ebf0446
Eenee0510 <p.enheenee@gmail.com>
2022-06-18 19:58:55 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -140,7 +140,7 @@ router.post("/lottery", function (req, res) {
      
       connection.query(search_query, function (err, result) {
         if (err) throw err;
-        res.send(result)
+        res.json(result[0])
         })
       });
     }); 
